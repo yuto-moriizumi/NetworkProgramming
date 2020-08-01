bc4ce5030611466d41d6b262d781b292ac630a0f
VolgaKurvar <kurvan1112@gmail.com>
2020-08-02 00:16:04 +0900

    全てを結合してselectするようにした

--- app/Http/Controllers/wc_resultController.php
+++ app/Http/Controllers/wc_resultController.php
@@ -9,6 +9,9 @@ use App\Http\Controllers\AppBaseController;
 use Illuminate\Http\Request;
 use Flash;
 use Response;
+use App\Models\wc_result;
+use App\Models\wc_tournament;
+use App\Models\wc_round;
 
 class wc_resultController extends AppBaseController
 {
@@ -29,10 +32,38 @@ class wc_resultController extends AppBaseController
      */
     public function index(Request $request)
     {
-        $wcResults = $this->wcResultRepository->all();
+        $team_id = $request->input('id');
+        $wcResults=null;
 
+        $search_info=[];
+        $search_info["tournaments"]=wc_tournament::all();
+        $search_option=[];
+        $search_option["tournament_id"]=$request->input('tournament_id');
+        if (!is_null($search_option["tournament_id"])) {
+            $search_info["rounds"]=wc_round::where('tournament_id', '=', $search_option["tournament_id"])->get();
+        }
+
+        if (is_null($team_id)) {
+            $wcResults = wc_result::select()->join('wc_match', 'wc_match.id', '=', 'wc_result.match_id')->join('wc_round', 'wc_round.id', '=', 'wc_match.round_id')->join('wc_tournament', 'wc_tournament.id', '=', 'wc_round.tournament_id')->join('wc_group', 'wc_group.id', '=', 'wc_match.group_id')->join('wc_team as wc_team0', 'wc_team0.id', '=', 'wc_result.team_id0')->join('wc_team as wc_team1', 'wc_team1.id', '=', 'wc_result.team_id1')->get();
+        } else { #チームIDが指定されている場合はそちらの検索を優先します
+            //$wcResults = wc_result::where('team_id0', '=', $team_id)->where('count_win', '=', 1)->get();
+            $wcResults = wc_result::whereRaw("team_id0 = ". $team_id ." AND count_win = 1 OR team_id1 = ". $team_id ." AND count_lose = 1")->get();
+
+            /*
+            $wcResults = wc_result::orWhere(function ($wcResults) use ($team_id) {
+                $wcResults->orWhere('team_id0', '=', $team_id)
+                      ->orWhere('count_win', '=', 1);
+            });*/
+
+            /*->orWhere(function ($query) use ($team_id) {
+                $query->where('column_id1', '=', $team_id)
+                      ->where('count_lose', '=', 1);
+            });
+            */
+        }
+        
         return view('wc_results.index')
-            ->with('wcResults', $wcResults);
+            ->with('wcResults', $wcResults)->with('val', $team_id)->with('search_info', $search_info);
     }
 
     /**
--- resources/views/wc_results/index.blade.php
+++ resources/views/wc_results/index.blade.php
@@ -7,12 +7,49 @@
            <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{{ route('wcResults.create') }}">Add New</a>
         </h1>
     </section>
+    
     <div class="content">
         <div class="clearfix"></div>
 
         @include('flash::message')
 
         <div class="clearfix"></div>
+        <form action="" method="GET">
+            {{ csrf_field() }}
+            <div>
+                <label>過去そのチームが勝利した試合の一覧を表示 IDを入力せよ</label><br>
+                <input type="number" name="id" />
+            </div>
+            <div>
+                <input type="submit" value="Create" />
+            </div>
+        </form>
+        <p>チームID勝利検索：<?=$val?></p>
+        <h2>高度な検索</h2>
+        <form action="" method="GET">
+            {{ csrf_field() }}
+            <div>
+                <label>大会で絞る</label><br>
+                <select name="tournament_id">
+                    <?php foreach ($search_info["tournaments"] as $option):?>
+                        <option value=<?=$option["id"]?>><?=$option["name"]?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+            <?php if (array_key_exists("rounds", $search_info)):?>
+                <div>
+                    <label>ラウンドで絞る</label><br>
+                    <select name="round_id">
+                        <?php foreach ($search_info["rounds"] as $option):?>
+                            <option value=<?=$option["id"]?>><?=$option["name"]?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>                
+            <?php endif; ?>
+            <div>
+                <input type="submit" value="Create" />
+            </div>
+        </form>
         <div class="box box-primary">
             <div class="box-body">
                     @include('wc_results.table')
@@ -23,4 +60,3 @@
         </div>
     </div>
 @endsection
-
